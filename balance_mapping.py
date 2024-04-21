369fd2f1eb1b4c1174cd81cfb4e1a241d929c7b5
seanyang0813 <32753704+seanyang0813@users.noreply.github.com>
2024-04-21 15:12:22 -0700

    get balance over time

--- balance_mapping.py
+++ balance_mapping.py
@@ -19,3 +19,29 @@ def get_balance(pub_key: str, blocks: List[Block]):
             if transaction.payee == pub_key:
                 balance += transaction.payment
     return balance
+
+def update_and_validate_balance_mapping(balance_mapping: Dict[str, float], block: Block):
+    # miner/transaction fee collection
+    if block.miner_pub_key not in balance_mapping:
+        balance_mapping[block.miner_pub_key] = 0
+    balance_mapping[block.miner_pub_key] += MINER_FEE
+    for transaction in block.transactions:
+        if transaction.payer not in balance_mapping:
+            raise Exception('payer does not have balance')
+        payer_balance = balance_mapping[transaction.payer]
+        payer_balance -= transaction.payment
+        payer_balance -= transaction.transaction_fee
+        if (payer_balance < 0):
+            raise Exception('payer balance is negative')
+        balance_mapping[transaction.payer] = payer_balance
+        if transaction.payee not in balance_mapping:
+            balance_mapping[transaction.payee] = 0
+        balance_mapping[transaction.payee] += transaction.payment
+        balance_mapping[block.miner_pub_key] += transaction.transaction_fee
+    return balance_mapping
+
+def get_all_balances(blocks: List[Block]):
+    balance_mapping = {}
+    for block in blocks:
+        balance_mapping = update_and_validate_balance_mapping(balance_mapping, block)
+    return balance_mapping
--- sample_run.py
+++ sample_run.py
@@ -1,8 +1,17 @@
 from blocks_reader import read_blocks
-from balance_mapping import get_balance
+from balance_mapping import get_balance, get_all_balances
 if __name__ == '__main__':
     blocks = read_blocks('sample-blocks.txt')
     for block in blocks:
         print(block)
     print(get_balance('PK2', blocks))
-    
+    print(get_balance('PK1', blocks))
+    print(get_all_balances(blocks))
+
+    blocks = read_blocks('sample-blocks-invalid.txt')
+    for block in blocks:
+        print(block)
+    try:
+        print(get_all_balances(blocks))
+    except Exception as e:
+        print(e)
