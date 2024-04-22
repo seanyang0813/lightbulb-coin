2cd0610573814ebed9ebf1e45c7e130809aea4a6
seanyang0813 <32753704+seanyang0813@users.noreply.github.com>
2024-04-21 19:12:44 -0700

    remove list of transactions from mempool api

--- miner_mempool_reader_writer.py
+++ miner_mempool_reader_writer.py
@@ -8,11 +8,32 @@ def read_mempool():
     for line in open('mempool/mempool.txt', 'r'):
         transactions.append(SignedTransaction.parse_transaction_line(line))
     return transactions
+
 def main():
     # read
     transactions = read_mempool()
     for t in transactions:
         print(t)
+    transactions_to_remove = [transactions[1]]
+    remove_finished_transactions_from_mempool(transactions_to_remove)
+
+def remove_finished_transactions_from_mempool(transactions_list):
+    # remove transactions that are finished from mempool
+    signature_to_remove_set = set()
+    for transaction in transactions_list:
+        signature_to_remove_set.add(transaction.sig1)
+    # open mempool txt 
+    with open('mempool/mempool.txt', 'r') as file:
+        lines = file.readlines()
+        # Filter lines; keep lines that do not start with '1'
+        filtered_lines = [line for line in lines if not line.split(' ')[-1] not in signature_to_remove_set]
+        # Write the filtered lines back to the file
+        with open('mempool/mempool.txt', 'w') as file:
+            file.writelines(filtered_lines)
+
+
+
+
 
 if __name__ == '__main__':
     main()
